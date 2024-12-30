d91de6da4205e047592b8d461ece1ca317f35fe5
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2024-12-31 00:20:34 +0530

    uploading docker compose

--- apache-php/index.php
+++ apache-php/index.php
@@ -1,31 +1,54 @@
 <?php
-$servername = "db";
-$username = "root";
-$password = "password";
-$dbname = "phpapp";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+$host = 'db';
+$user = 'root';
+$pass = 'password';
+$conn = new mysqli($host, $user, $pass);
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-// sql to create table
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
-
-if ($conn->query($sql) === TRUE) {
-  echo "Table MyGuests created successfully";
+   die("Connection failed: " . $conn->connect_error);
 } else {
-  echo "Error creating table: " . $conn->error;
+   echo "Connected to MySQL server successfully!";
 }
-
-$conn->close();
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Create Record</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+<style type="text/css">
+.wrapper{
+  width: 500px;
+  margin: 0 auto;
+}
+</style>
+</head>
+<body>
+<div class="wrapper">
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-12">
+<div class="page-header">
+<h2>Contact Form</h2>
+</div>
+<p>Please fill this form and submit to add employee record to the database.</p>
+<form action="insert.php" method="post">
+<div class="form-group">
+<label>Name</label>
+<input type="text" name="name" class="form-control">
+</div>
+<div class="form-group">
+<label>Email</label>
+<input type="email" name="email" class="form-control">
+</div>
+<div class="form-group">
+<label>Mobile</label>
+<input type="mobile" name="mobile" class="form-control">
+</div>
+<input type="submit" class="btn btn-primary" name="submit" value="Submit">
+</form>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
