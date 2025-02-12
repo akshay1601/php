eaa71a97f5374559b9d3a479e3d7e603cbc79dc2
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-02-12 23:23:23 +0530

    uploading docker compose

--- apache-php/index.php
+++ apache-php/index.php
@@ -16,3 +16,19 @@
     </form>
 </body>
 </html>
+
+<?php
+
+$host = 'db';
+$user = 'root';
+$pass = 'password';
+
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+} else {
+   echo "Connected to MySQL server successfully!";
+}
+?>
+
+
