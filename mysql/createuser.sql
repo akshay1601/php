c234fa084483ba943cfb77b37432f0a9fa908101
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-02-13 20:14:30 +0530

    uploading docker compose

---
+++ mysql/createuser.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE login_system;
+USE login_system;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Insert a sample user (username: admin, password: admin123)
+INSERT INTO users (username, password) VALUES ('akki', '$6$GHqYc96XStM30Rry$MppMl29Nvl0G/vL.I6Xv7fx.R7fKSZALmFdeeDHVZaMcEpie0y78JCWwMeDl57ZwQ7jv20KmWOwaOvGlngqww1');
