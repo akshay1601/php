63517d8db82e202bac4d877910ea10c2b8341db7
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-02-13 20:37:42 +0530

    uploading docker compose

--- mysql/sql-script/createuser.sql
+++ mysql/sql-script/createuser.sql
@@ -9,3 +9,9 @@ CREATE TABLE users (
 
 -- Insert a sample user (username: admin, password: admin123)
 INSERT INTO users (username, password) VALUES ('akki', '$6$GHqYc96XStM30Rry$MppMl29Nvl0G/vL.I6Xv7fx.R7fKSZALmFdeeDHVZaMcEpie0y78JCWwMeDl57ZwQ7jv20KmWOwaOvGlngqww1');
+
+-- docker stop db php-apache
+-- docker rm -f php-apache  db
+-- docker rmi -f akshayv1601/php:3 akshayv1601/mysql:3
+-- sudo rm -rf php/ server3-config.sh 
+-- sudo rm -rf /var/lib/mysql/
